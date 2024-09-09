23151f3b3cf7b4c9aad53f271607d5e0c730317b
juDemarco <89648057+juDemarco@users.noreply.github.com>
2024-09-09 13:08:03 -0300

    Update transpose.py

--- matrix/transpose.py
+++ matrix/transpose.py
@@ -1,2 +1,9 @@
 def transpose(matrix):
+    nuevaMat=[]
+    
+    for n in range(len(matrix[0])):
+        nuevaMat.append([])
+        for k in range(len(matrix)):
+            nuevaMat[n].append(matrix[k][n])
+    return nuevaMat
     pass

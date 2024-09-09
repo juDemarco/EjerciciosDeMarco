e785b7067374cedbda0847f8697798b8eebf18fa
juDemarco <89648057+juDemarco@users.noreply.github.com>
2024-09-09 00:11:02 -0300

    Update circle.py

--- circle/circle.py
+++ circle/circle.py
@@ -1,4 +1,27 @@
 import math
 
 class Circle():
+    def __init__(self,radius):
+        if(radius<=0):
+            raise ValueError("El valor del radio tiene que ser mayor a 0")
+        
+        self.radius=radius
+    
+    def get_radius(self):
+        return self.radius
+    
+    def get_area(self):
+        return math.pi * math.pow(self.radius,2)
+    
+    def get_perimeter(self):
+        return 2 *math.pi * self.radius
+    
+    def __mul__(self, n):
+        
+        if(n<=0):
+            raise ValueError("El valor del multiplo tiene que ser mayor a 0")
+        newCircle = Circle(self.radius * n)
+        
+        return newCircle
+        
     pass
